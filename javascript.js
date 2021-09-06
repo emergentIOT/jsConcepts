97cf1fd4b0940b371130bfd8b673adce7d0e20d1
Amandeep Singh <Amandeep.Singh4@transport.nsw.gov.au>
2021-09-06 19:10:22 +1000

    What is Javascript

---
+++ javascript.js
@@ -0,0 +1,39 @@
+/**
+ * JS is a Synchronous Single Threaded language.
+ * Everything happend in JAVSCRIPT is inside EXECUTION CONTEXT.
+ * 
+ * Where EC, is divided into two parts.
+ * 
+ *  1.Memory(Environment Variable) ---> PHAZE 1
+ *      - Where we save, key:value;
+ *                       a = 10;
+ *                       functions() {}
+ *  2.Code (Thread of Execution) ---> PHAZE 2
+ *      - Code is executed line by line.
+ */
+
+
+/**
+ * PHAZE 1 
+ * n = undefined 
+ * //Number is stored as it is
+ * number(num) {
+    var ans = num * num;
+    return ans;
+    } //For function execution it will create aother execution context, same as explained.
+    square = undefined;
+    square1 = undefined;
+
+    Phase 1 is completed and phaze 2 will start i.e. code execution line by line.
+ */
+var n = 2;
+function number(num) {
+    var ans = num * num;
+    return ans;
+}
+
+var square = number(n);
+var square2 = number(4);
+
+
+
