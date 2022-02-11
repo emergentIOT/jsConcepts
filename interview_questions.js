79956c251f324d34f296ad908bd6b66e2022b04f
emergentIOT <aman24january@gmail.com>
2022-02-11 16:04:11 +1100

    Interview questions

---
+++ interview_questions.js
@@ -0,0 +1,61 @@
+/**
+ * Commonwealth Bank Of Australia, 04 Feb 2022 - Software Engineer
+ * 
+ * 1. Two types of encryption ?
+ * 
+ * Symmetric & Asymmetric encryption
+ * 
+ * Symmetric - encryption where same keys are use to encrypt & decrypt.
+ * 
+ * Asymmetric - it is a key pair encrytion , where public & private keys 
+ *              used to encrypt & decrypt.
+ *              Public key is used to encrypt the message/data and private key is 
+ *              used to decrypt the message.
+ * -----------------------------------------------------------------------------------
+ * - Encryption vs Hashing
+ *      Both job is to secure password.
+ * 
+ * Encryption - can encode message & decode message
+ * We can encode and decode text, eg. aman(encode) -> udhwjmn(decode) -> aman
+ *      
+ * Hashing - One way communication 
+ * eg. aman -> anmnjkfhdkf.dsofuewn.oiutshd    
+ * 
+ * 
+ * 
+ *  
+ * 
+ * 2. Hoisting ?
+ *    see file 2_hoisting_in_js.js
+ * 
+ * 3. Authentication & Authorization ?
+ * 
+ * -  Authentication is a process where users validate themselves,
+ *    eg: Username & password
+ *        one time password 
+ *        fingerprints
+ * -  Authorization is a process where special permission grant to user and limited resources
+ *    are alotted to user. Also known as access control or client proviledges.
+ *    eg: Adding a user in group, giving admin rights to user.
+ * 
+ * In secure environements, authorization must always follows authentication. Eg. In HCL , i proved 
+ * my identity first (Authentication) then i got priviedges to access the applications (Authorization). 
+ * 
+ * Read More: https://www.okta.com/identity-101/authentication-vs-authorization/
+ *    
+ * 4. Single sign on ?
+ * 5. Difference between import & require ?
+ * 6. Dependency Injection ?
+ * 7. Layers in web app ?
+ * 8. Solid principles  ?
+ * 9. Exception handling and how to implement for mongodb & nodejs ?
+ * 10. What is REST api & verbs ?
+ * 11. What to do if database is out of resources ?
+ * 12. How to pass reference in JS ?
+ * 13. Services in Angular ?
+ * 14. Angular architecture ?
+ * 15. Code patterns ?
+ * 16. What is JWT ?
+ * 
+ * 
+ */
